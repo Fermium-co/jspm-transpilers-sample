fcdb416c5ca0cc20e39411b57e2b70885f79fd21
Yasser Moradi <y.moradi@outlook.com>
2015-09-26 10:53:48 +0330

    configure typescript transpiler

--- config.js
+++ config.js
@@ -6,7 +6,26 @@ System.config({
     "github:*": "jspm_packages/github/*",
     "npm:*": "jspm_packages/npm/*"
   },
-
+  typescriptOptions: { /* typescript options */
+    "noImplicitAny": false,
+    "typeCheck": true,
+    "experimentalDecorators": true,
+    "module": "system",
+    "emitDecoratorMetadata": true,
+    "experimentalAsyncFunctions": true,
+    "taget": "ES5"
+  },
+  packages: {
+    "src": { /* src folder */
+      "main": "index", /* index.ts file in src folder */
+      "defaultExtension": "ts",
+      "meta": {
+        "*.ts": {
+          "loader": "ts"
+        }
+      }
+    },
+  },
   map: {
     "ts": "github:frankwallis/plugin-typescript@2.0.17",
     "github:frankwallis/plugin-typescript@2.0.17": {
