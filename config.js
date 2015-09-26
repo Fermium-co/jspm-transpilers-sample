e1bb2e28f038f627c44618cc5b8845b3c706384f
Yasser Moradi <y.moradi@outlook.com>
2015-09-26 10:50:44 +0330

    added ts to jspm

--- config.js
+++ config.js
@@ -2,5 +2,15 @@ System.config({
   baseURL: "/",
   defaultJSExtensions: true,
   transpiler: "none",
-  paths: {}
+  paths: {
+    "github:*": "jspm_packages/github/*",
+    "npm:*": "jspm_packages/npm/*"
+  },
+
+  map: {
+    "ts": "github:frankwallis/plugin-typescript@2.0.17",
+    "github:frankwallis/plugin-typescript@2.0.17": {
+      "typescript": "npm:typescript@1.6.2"
+    }
+  }
 });
