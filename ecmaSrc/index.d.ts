8fa443b359cafd3110513cb85129b25a385e6903
Yasser Moradi <y.moradi@outlook.com>
2015-09-26 13:54:12 +0330

    Enhanced d.ts defenition for index.js

--- ecmaSrc/index.d.ts
+++ ecmaSrc/index.d.ts
@@ -1,5 +1,5 @@
 // with this definition, we can provide intelliscene to our typescript files about index.js.
 
 export class EcmaBasedClass {
-	static show(); // This is a definition, there is no need to rewrite implementation again here.
+	static show(): void; // This is a definition, there is no need to rewrite implementation again here.
 }
